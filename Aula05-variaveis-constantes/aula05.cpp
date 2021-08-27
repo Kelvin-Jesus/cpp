b608ef9b9e5097bf1506f95cdf94d4f4b8a74af1
Kelvin Jesus <kj.theprog@gmail.com>
2021-08-27 09:22:11 -0300

    Aula05 - Constantes e varíáveis múltiplas

---
+++ Aula05-variaveis-constantes/aula05.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define canal cout << "Curso";
+
+int main() {
+
+    int vidas = 3, tiros = 500, life = 100;
+    // int vidas; int tiros; int life;
+
+    cout << pi << "\n";
+    canal;
+
+    return 0;
+}
